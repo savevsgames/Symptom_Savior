b402bf4368cb8bc71e8c7ced0d3940efb797269d
Greg Barker <126988787+savevsgames@users.noreply.github.com>
2025-06-19 04:52:32 -0600

    Updated babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    // Note: expo-router/babel plugin is deprecated in Expo Router 4.x
-    // The router preset handles this automatically
+    plugins: ['expo-router/babel'],
   };
 };
