23730120f396aa379c35f92ad766c6231f85cc72
Greg Barker <126988787+savevsgames@users.noreply.github.com>
2025-06-19 03:15:44 -0600

    Updated babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],   // ✅ one-stop shop
-    // plugins: ['expo-router/babel'], // ❌ delete this line and never add it again - deprecated
+    // plugins: ['expo-router/babel'], // ❌ deleted this line and never add it again - deprecated
   };
 };
